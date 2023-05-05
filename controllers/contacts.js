20f27798c6098529700d88e1dd4cec3fde6bc294
jordankraude <jordankraudeTP@gmail.com>
2023-05-05 10:45:38 -0600

    puhsing everything

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -20,6 +20,52 @@ const getOne = async (req, res, next) => {
   })
 }
 
-module.exports = {getAll, getOne}
+const createContact = async (req, res, next) => {
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday}
+  const result = await mongodb.getDb().db('week2').collection('WebServicesCollection').insertOne(contact);
+  console.log(result);
+  if (result.acknowledged) {
+    res.status(201).json(result);
+  } 
+  else {
+    res.status(500).json(result.error)}
+}
+
+const updateContact = async (req, res, next) => {
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday}
+  const userId = new UserId (req.params.id)
+  const result = await mongodb.getDb().db('week2').collection('WebServicesCollection').replaceOne({_id: userId}, contact);
+  console.log(result);
+  if (result.modifiedCount > 0) {
+    res.status(204).send();
+  } 
+  else {
+    res.status(500).json(result.error)}
+  }
+
+
+  const deleteContact = async (req, res, next) => {
+    const userId = new UserId(req.params.id);
+    const result = await mongodb.getDb().db('week2').collection('WebServicesCollection').remove({ _id: userId }, true);
+    console.log(result);
+    if (result.deletedCount > 0) {
+      res.status(200).send();
+    } 
+    else {
+      res.status(500).json(result.error);
+    }
+  };
+
+module.exports = {getAll, getOne, createContact, deleteContact, updateContact}
 
 // 
--- routes/contacts.js
+++ routes/contacts.js
@@ -6,5 +6,11 @@ const contactsController = require('../controllers/contacts');
 router.get('/', contactsController.getAll);
 router.get('/:id', contactsController.getOne)
 
+router.post('/', contactsController.createContact);
+
+router.put('/:id', contactsController.updateContact);
+
+router.delete('/:id', contactsController.deleteContact);
+
 
 module.exports = router;
